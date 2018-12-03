75125a8370e9695c266050a10854e15dffe4b1e5
Tasin <tasin@Tasins-MacBook-Air.local>
2018-12-03 09:09:29 +0000

    second page

---
+++ app/src/main/java/com/example/tasin/idateu/Over_18_confirm.java
@@ -0,0 +1,13 @@
+package com.example.tasin.idateu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Over_18_confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over_18_confirm);
+    }
+}
