d18c12bccaca785d1d0f0a63f5d93f6958ce2a56
1702609 <1702609@brunel.ac.uk>
2018-12-11 15:40:33 +0000

    third page progress - NOT COMPLETE

--- app/src/main/java/com/dating/app/idateu/ThirdSignUpPage.java
+++ app/src/main/java/com/dating/app/idateu/ThirdSignUpPage.java
@@ -1,21 +1,56 @@
 package com.dating.app.idateu;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.tasin.idateu.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ThirdSignUpPage extends AppCompatActivity {
+
+    EditText date;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_sign_up_page);
 
+        date = (EditText) findViewById(R.id.date);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(ThirdSignUpPage.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
 
     }
-}
+    }
+
