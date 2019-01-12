ed3bc531235b6d5a94c3a5f96f9bec636b605b3e
1702609 <1702609@brunel.ac.uk>
2019-01-12 20:58:47 +0000

    homepage improved

--- app/src/main/java/com/dating/app/idateu/Homepage/HomePage.java
+++ app/src/main/java/com/dating/app/idateu/Homepage/HomePage.java
@@ -1,43 +1,57 @@
 package com.dating.app.idateu.Homepage;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.Gallery;
 import android.widget.ImageView;
 
 import com.dating.app.idateu.R;
-import com.dating.app.idateu.SignUp_LogIn.SecondSignUpPage;
-import com.dating.app.idateu.SignUp_LogIn.ThirdSignUpPage;
+
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
     ImageView current_match;
     Button like_button;
+    int number_of_clicks = 0;
 
 
+    private ArrayList<Integer> matchImages = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        matchImages.add(R.drawable.img_001);
+        matchImages.add(R.drawable.img_002);
+        matchImages.add(R.drawable.img_003);
+
         current_match=(ImageView)findViewById(R.id.match_pic);
-        current_match.setImageResource(R.drawable.dummy_pic);
+        current_match.setImageResource(matchImages.get(0));
         like_button=(Button) findViewById(R.id.like_btn);
         like_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                current_match.setImageResource(R.drawable.water_girl);
-            }
+            public void onClick(View view)
+                {
+                if (matchImages.size() != number_of_clicks+1)
+                    {
+                    number_of_clicks++;
+                    current_match.setImageResource(matchImages.get(number_of_clicks));
+                    }
+                else
+                    {
+                    number_of_clicks = 0;
+                    current_match.setImageResource(matchImages.get(number_of_clicks));
+                    }
+                }
             });
         }
+
+
+
 
     }
 
