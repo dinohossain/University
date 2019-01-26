56604673c0b5b3f2aa9d03e89dde68217df99c60
1702609 <1702609@brunel.ac.uk>
2019-01-26 13:49:11 +0000

    code is more organised

--- app/src/main/java/com/dating/app/idateu/Homepage/HomePage.java
+++ app/src/main/java/com/dating/app/idateu/Homepage/HomePage.java
@@ -1,8 +1,6 @@
 package com.dating.app.idateu.Homepage;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +9,14 @@ import android.widget.ImageView;
 
 import com.dating.app.idateu.R;
 import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
 
-
+    int matchPicIndex = 0;
     ImageView current_match;
     Button like_button, dislike_button;
-    int matchPicIndex = 0;
 
 
     private ArrayList<Integer> matchImages = new ArrayList<Integer>();
@@ -29,45 +25,59 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        matchImages.add(R.drawable.img_001);
-        matchImages.add(R.drawable.img_002);
-        matchImages.add(R.drawable.img_003);
-
         current_match=(ImageView)findViewById(R.id.match_pic);
         ImageView image = new ImageView(this);
+        initialImages();
         Picasso.get().load(matchImages.get(0)).into(current_match); //loads initial image
-        like_button=(Button) findViewById(R.id.like_btn);
 
+        like_button=(Button) findViewById(R.id.like_btn);
         dislike_button = findViewById(R.id.dislike_btn);
 
         like_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
                 {
-                if (matchImages.size() != matchPicIndex +1)
-                    {
-                    matchPicIndex++;
-                    Picasso.get().load(matchImages.get(matchPicIndex)).into(current_match);
-                    }
-                else
-                    {
-                    matchPicIndex = 0;
-                    Picasso.get().load(matchImages.get(matchPicIndex)).into(current_match);
-                    }
+                imageChanger();
                 }
             });
 
         current_match.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent start_popup = new Intent(HomePage.this, Pop_up.class);
-                start_popup.putExtra("picture_ID", matchImages.get(matchPicIndex));
-                startActivity(start_popup);
-            }
-        });
-
+            public void onClick(View view)
+                {
+                startPop_up();
+                }
+                });
         }
 
-    }
+        private void imageChanger()
+            {
+            if (matchImages.size() != matchPicIndex +1)
+                {
+                matchPicIndex++;
+                Picasso.get().load(matchImages.get(matchPicIndex)).into(current_match);
+                }
+            else
+                {
+                matchPicIndex = 0;
+                Picasso.get().load(matchImages.get(matchPicIndex)).into(current_match);
+                }
+            }
+
+    private void initialImages()
+            {
+            matchImages.add(R.drawable.img_001);
+            matchImages.add(R.drawable.img_002);
+            matchImages.add(R.drawable.img_003);
+            }
+
+    private void startPop_up()
+        {
+        Intent start_popup = new Intent(HomePage.this, Pop_up.class);
+        start_popup.putExtra("picture_ID", matchImages.get(matchPicIndex));
+        startActivity(start_popup);
+        }
+
+}
 
 
