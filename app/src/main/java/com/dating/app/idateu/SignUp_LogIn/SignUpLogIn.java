3837e124ab282808d3b131f317a38a0ccf79f9ec
1702609 <1702609@brunel.ac.uk>
2019-02-26 15:35:47 +0000

    Signup page checks if email and pass is valid, error message gets displayed login detail dont exist

--- app/src/main/java/com/dating/app/idateu/SignUp_LogIn/SignUpLogIn.java
+++ app/src/main/java/com/dating/app/idateu/SignUp_LogIn/SignUpLogIn.java
@@ -3,10 +3,10 @@ package com.dating.app.idateu.SignUp_LogIn;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.dating.app.idateu.Homepage.HomePage;
 import com.dating.app.idateu.R;
@@ -16,6 +16,8 @@ import java.util.regex.Pattern;
 
 public class SignUpLogIn extends AppCompatActivity
     {
+    String fakeEmail = "t@g.com";
+    String fakePass = "test";
 
     Button register_btn;
     Button login_btn;
@@ -23,12 +25,15 @@ public class SignUpLogIn extends AppCompatActivity
     EditText email_edit;
     EditText pass_edit;
 
+    TextView noRecordMsg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_view);
-
+        noRecordMsg=findViewById(R.id.no_record);
+        noRecordMsg.setVisibility(View.INVISIBLE);
         email_edit = findViewById(R.id.emailInput);
         pass_edit = findViewById(R.id.passwordInput);
 
@@ -46,30 +51,28 @@ public class SignUpLogIn extends AppCompatActivity
                 @Override
                 public void onClick(View view)
                 {
-                startActivity(new Intent(SignUpLogIn.this, HomePage.class));
+                if(noUserError()) startActivity(new Intent(SignUpLogIn.this, HomePage.class));
                 }
             });
         }
 
-        private boolean isThereAError()
+        private boolean noUserError()
             {
             boolean status = false;
             String email = email_edit.getText().toString();
             String password = pass_edit.getText().toString();
-            if (isEmailValid(email)==false)
+            if (isEmailValid(email)
+                && didUserTypeInPassword(password)
+                && doesRecordExistInDB(email,password))
                 {
-                    email_edit.setError("Invalid Email");
-                    return status = true;
-                }
-            if (doesRecordExistInDB(email,password))
-                {
-
+                status = true;
                 }
             return status;}
 
         private boolean doesRecordExistInDB(String email, String password)
             {
-
+            if (email.equals(fakeEmail) && password.equals(fakePass)) return true;
+            noRecordMsg.setVisibility(View.VISIBLE);
             return false;
             }
 
@@ -79,7 +82,19 @@ public class SignUpLogIn extends AppCompatActivity
                         Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
                 Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(s);
                 boolean stat = matcher.find() == true ? true : false;
+                if (!stat) email_edit.setError("Invalid Email");
                 return stat;
             }
+
+        private boolean didUserTypeInPassword(String password)
+            {
+            if (password.length() > 0) return true;
+            else
+                {
+                pass_edit.setError("Enter your password");
+                return false;
+                }
+            }
+
 
     }
